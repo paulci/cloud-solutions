dc680707040d14cc8f200258504665c889d136f2
Paul Cieslar <paul@cieslar.org.uk>
2021-10-16 23:15:20 +0100

    cleanup, further testing.  WAF and tuning remain

--- aws/restful-api/auth-function/lambda_function.py
+++ aws/restful-api/auth-function/lambda_function.py
@@ -1,26 +1,82 @@
+import base64
+import os
+
 import jwt
 from jwt.exceptions import PyJWTError
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = os.environ.get('jwt_secret_arn')
+    region_name = os.environ.get('region')
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            
+    return secret
 
 
 def lambda_handler(event, context):
+    print('Event: {}'.format(event))
+    print('Context: {}'.format(context))
 
     policy = {
-        "principalId": "user",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
+        'principalId': 'user',
+        'policyDocument': {
+            'Version': '2012-10-17',
+            'Statement': [
                 {
-                    "Action": "execute-api:Invoke",
-                    "Effect": "Deny",
-                    "Resource": "arn:aws:execute-api:us-east-1:198653053291:y0je959838/*/GET/paul"
+                    'Action': 'execute-api:Invoke',
+                    'Effect': 'Deny',
+                    'Resource': event['methodArn']
                 }
             ]
         }
     }
     encoded_payload = event['authorizationToken']
+    secret = get_secret()
     
     try:
-        payload = jwt.decode(encoded_payload, 'mysecret', algorithms=['HS256'])
+        payload = jwt.decode(encoded_payload, secret, algorithms=['HS256'])
     except PyJWTError:
         return policy
         
--- aws/restful-api/main.tf
+++ aws/restful-api/main.tf
@@ -15,8 +15,8 @@ resource "aws_api_gateway_method" "modularresourcemethod" {
   rest_api_id      = aws_api_gateway_rest_api.modularapi.id
   resource_id      = aws_api_gateway_resource.modularresource.id
   http_method      = "GET"
-  authorization    = "CUSTOM" # CUSTOM for lambda
-  authorizer_id    = aws_api_gateway_authorizer.demo.id
+  authorization    = "CUSTOM"
+  authorizer_id    = aws_api_gateway_authorizer.python_jwt_auth.id
   api_key_required = false
 }
 
@@ -29,8 +29,8 @@ resource "aws_api_gateway_integration" "lambdaintegration" {
   uri                     = aws_lambda_function.hello.invoke_arn
 }
 
-resource "aws_api_gateway_authorizer" "demo" {
-  name                             = "demo"
+resource "aws_api_gateway_authorizer" "python_jwt_auth" {
+  name                             = "python_jwt_auth"
   rest_api_id                      = aws_api_gateway_rest_api.modularapi.id
   authorizer_uri                   = aws_lambda_function.authorizer.invoke_arn
   authorizer_result_ttl_in_seconds = 60
@@ -77,8 +77,24 @@ resource "aws_iam_role_policy" "invocation_policy" {
 EOF
 }
 
-resource "aws_iam_role" "lambda" {
-  name = "demo-lambda"
+resource "aws_iam_policy" "secrets_policy" {
+  name = "default"
+
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "secretsmanager:GetSecretValue",
+        "Effect" : "Allow",
+        "Resource" : "${aws_secretsmanager_secret.jwtkey.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "lambda_execution" {
+  name = "api-lambda-execution"
 
   assume_role_policy = <<EOF
 {
@@ -100,16 +116,22 @@ EOF
 resource "aws_lambda_function" "authorizer" {
   function_name    = "mypythonauth"
   filename         = "auth-function.zip"
-  role             = aws_iam_role.lambda.arn
+  role             = aws_iam_role.lambda_execution.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.8"
   source_code_hash = data.archive_file.auth.output_base64sha256
+  environment {
+    variables = {
+      jwt_secret_arn = aws_secretsmanager_secret.jwtkey.arn
+      region         = data.aws_region.current.name
+    }
+  }
 }
 
 resource "aws_lambda_function" "hello" {
   function_name    = "web"
   filename         = "hello-function.zip"
-  role             = aws_iam_role.lambda.arn
+  role             = aws_iam_role.lambda_execution.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
   source_code_hash = data.archive_file.web.output_base64sha256
@@ -120,14 +142,16 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.hello.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:198653053291:${aws_api_gateway_rest_api.modularapi.id}/*/${aws_api_gateway_method.modularresourcemethod.http_method}${aws_api_gateway_resource.modularresource.path}"
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.modularapi.id}/*/${aws_api_gateway_method.modularresourcemethod.http_method}${aws_api_gateway_resource.modularresource.path}"
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 data "archive_file" "auth" {
   type        = "zip"
   source_dir  = "auth-function"
   output_path = "auth-function.zip"
-  excludes    = ["auth-function/requirements.txt"]
+  excludes    = ["requirements.txt"]
 }
 
 data "archive_file" "web" {
@@ -136,7 +160,22 @@ data "archive_file" "web" {
   output_path = "hello-function.zip"
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.lambda.name
+resource "aws_iam_role_policy_attachment" "cloudwatch-lambda-attach" {
+  role       = aws_iam_role.lambda_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "secrets-lambda-attach" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = aws_iam_policy.secrets_policy.arn
+}
+
+resource "aws_secretsmanager_secret" "jwtkey" {
+  name        = "JWTSecret001"
+  description = "JWT Secret for Lambda Authorizer"
+}
+
+resource "aws_secretsmanager_secret_version" "jwtkey" {
+  secret_id     = aws_secretsmanager_secret.jwtkey.id
+  secret_string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoicGF1bCJ9.dnFzd_5kMxN41MqahA0X1ZzvMnpEioPDGjDL_xcqd5Y"
 }
